c1e6bef8483aaff7a5c6353b6b85eaa6d6f85594
Louis Morka <louismorka02006@gmail.com>
2025-05-23 13:23:52 +0100

    a high level overview of dart

---
+++ variables.dart
@@ -0,0 +1,16 @@
+String name = " Jane Doe";
+int amount = 23;
+double figure = 23.9;
+bool isAugust = true;
+String sentence = "hello world";
+String word = "learning dart";
+
+
+
+void main(List <String> args) {
+print (name);
+print(amount);
+print(isAugust);
+print(sentence);
+print(word);
+}
